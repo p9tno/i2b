57e298c7b89feae1b6349809c0f5c7dd6e2032ea
p9tno <plahotin_s@mail.ru>
2023-04-30 22:37:42 +0300

    start add animate

--- js/function.js
+++ js/function.js
@@ -33,6 +33,7 @@ function isTouch() {
 
 window.onload = function () {
     console.log('onload');
+    $('.animate-js').removeClass('aos-animate')
     function preloader() {
         $(()=>{
 
@@ -43,26 +44,24 @@ window.onload = function () {
                 setTimeout( () => {
                     p.remove()
                 },1000);
+                $('.animate-js').addClass('aos-animate');
 
             },1000);
         });
     }
     preloader();
+
     // setTimeout( ()=> preloader(),15000 )
 }
 
 $(document).ready(function() {
     console.log('ready');
 
-    function toggleContent() {
+    function toggleNav() {
         $('.toggle').click(function() {
-            console.log('toggleContent');
-
-            // let wrap = $(this);
             let toggle = $('.toggle__button');
             let content = $('.header__nav');
             let close = $('.navbar__close');
-
 
             $( 'body' ).addClass( 'nav-open' );
             content.addClass('active');
@@ -74,34 +73,12 @@ $(document).ready(function() {
 
         });
     };
-    toggleContent();
-
-    function doDrop() {
-
-        $('.drop__toggle').on('click', function() {
-            console.log('clock');
-            $('.drop__toggle').not(this).removeClass('active');
-            $('.drop__toggle').not(this).closest('.drop').find('.menu').removeClass('open');
-            $(this).toggleClass('active');
-            $(this).closest('.drop').find('.menu').toggleClass('open');
-        });
-
-        $(document).mouseup(function (e) {
-            let div = $(".drop__toggle");
-            if (!div.is(e.target) && div.has(e.target).length === 0) {
-                div.removeClass('active');
-                div.closest('.drop').find('.menu').removeClass('open');
-            }
-        });
-    };
-    doDrop();
-
+    toggleNav();
 
     function showModal() {
         $('.show_modal_js').on('click', function (e) {
             e.preventDefault();
             let id  = $(this).attr('href');
-
             $(id).modal('show');
         });
 
@@ -114,110 +91,6 @@ $(document).ready(function() {
         });
     }
     showModal();
-
-
-    function openMobileNav() {
-        $('.header__toggle').click(function(event) {
-            // console.log('Показ меню');
-            $('.navbar').toggleClass('navbar_open');
-            $('.header__toggle').toggleClass('header__toggle_open');
-            $( 'body' ).toggleClass( 'nav-open' );
-        });
-    };
-    openMobileNav();
-
-    function activeNav() {
-        $('.menu-item').on('click', function() {
-            $('.menu-item').removeClass('current-menu-item');
-            $(this).addClass('current-menu-item');
-        })
-    };
-    activeNav();
-
-    function showMore(classItem, btn) {
-        // let classItem = '.vacancies__item';
-        // let classItem = class;
-        let item = $(''+ classItem +'');
-        let count = item.length;
-        let start = 1;
-        let show = 1;
-
-        item.addClass('d-none');
-        $('' + classItem + ':lt(' + start + ')').removeClass('d-none');
-
-        $(btn).click(function(e) {
-            e.preventDefault();
-            $(this).addClass('loading');
-
-            let load = $(this).data('load');
-            let more = $(this).data('more');
-
-            start = (start + show <= count) ? start + show : count;
-
-            $(this).text(load);
-
-            setTimeout(() => {
-                $(''+ classItem +':lt(' + start + ')').removeClass('d-none');
-                if ($(''+ classItem +':not(.d-none)').length == count) {
-                    $(this).parent().remove();
-                }
-                $(this).removeClass('loading');
-                $(this).text(more);
-            }, 500);
-        });
-
-    }
-    // showMore('.vacancies__item', '.show_more_v_js');
-
-    function collapsed() {
-        let toggle = $('[data-collapse]');
-
-        toggle.on('click', function() {
-            let id = $(this).data('collapse'),
-            body = $('[data-collapse-body="'+id+'"]'),
-            wrap = body.closest('[data-collapse-wrapper]');
-
-            if (!id) {
-                // $('[data-collapse-wrapper]').removeClass('open');
-                body = $(this).parent().find('[data-collapse-body]');
-                $(this).toggleClass('open');
-                if ($(this).hasClass('open')) {
-                    body.slideDown();
-                } else {
-                    body.slideUp();
-                }
-            } else if (id === 'all') {
-                body.slideDown();
-                toggle.addClass('open');
-            } else {
-                body.slideToggle();
-                $(this).toggleClass('open');
-            }
-        });
-    }
-    collapsed();
-
-    function doTabs () {
-        $('.tabs__wrapper').each(function() {
-            let ths = $(this);
-            ths.find('.tab__item').not(':first').hide();
-            ths.find('.tab').click(function() {
-                ths.find('.tab').removeClass('active').eq($(this).index()).addClass('active');
-                ths.find('.tab__item').hide().eq($(this).index()).fadeIn()
-            }).eq(0).addClass('active');
-        });
-    }
-    doTabs();
-
-    // function doDrop() {
-    //     $('.drop__toggle').on('click', function() {
-    //         // $('.drop__list').toggleClass('open');
-    //         $(this).toggleClass('active');
-    //         $(this).closest('.drop').find('.drop__list').toggleClass('open');
-    //     });
-    // };
-    // doDrop();
-
 
     $(function(){
         $(".tel").mask("+7 ( 999 ) 999 - 99 - 99");
@@ -251,51 +124,6 @@ $(document).ready(function() {
         ],
     });
 
-
-    // start animate numbers
-    function onVisible( selector, callback, repeat = false ) {
-
-    let options = {
-        threshold: [ 0.5 ]
-    };
-    let observer = new IntersectionObserver( onEntry, options );
-    let elements = document.querySelectorAll( selector );
-
-    for ( let elm of elements ) {
-        observer.observe( elm );
-    }
-
-    function onEntry( entry ) {
-        entry.forEach( change => {
-            let elem = change.target;
-            // console.log(change);
-            // console.log(elem.innerHTML);
-            if ( change.isIntersecting ) {
-                if ( !elem.classList.contains( 'show' ) || repeat ) {
-                    elem.classList.add( 'show' );
-                    callback( elem );
-                }
-            }
-        } );
-    }
-    }
-
-    onVisible( '.programsInfo__number', function ( e ) {
-        animateNumber( e, e.innerHTML );
-    } );
-
-    function animateNumber( elem, final, duration = 1000 ) {
-        let start = 0;
-        // console.log('init');
-        setInterval( function () {
-            if ( final > start ) {
-                elem.innerHTML = start++;
-            }
-        }, duration / final );
-    }
-    // end animate numbers
-
-
     function initAOS () {
         // https://github.com/michalsnik/aos
         AOS.init({
@@ -303,17 +131,17 @@ $(document).ready(function() {
             // anchorPlacement: 'bottom-bottom',
             duration: 1000, // values from 0 to 3000, with step 50ms
             // offset: 20,
-            once: true,
+            // once: true,
         });
 
         AOS.init({
             disable: function () {
-                var maxWidth = 768;
+                var maxWidth = 1365;
                 return window.innerWidth < maxWidth;
             }
         });
 
     }
-    // initAOS ();
+    initAOS ();
 
 })
